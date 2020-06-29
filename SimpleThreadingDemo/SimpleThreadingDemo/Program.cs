716d5d9513d912d4276639018789ab6f5db80a73
Dan Dumitriu <dandumitriu33@gmail.com>
2020-06-29 18:30:10 +0300

    Add SimpleThreadingDemo solution

--- SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
+++ SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace SimpleThreadingDemo
 {
@@ -10,6 +10,22 @@ namespace SimpleThreadingDemo
     {
         static void Main(string[] args)
         {
+            Thread one = new Thread(new ThreadStart(Counting));
+            Thread two = new Thread(new ThreadStart(Counting));
+
+            one.Start();
+            two.Start();
+            one.Join();
+            two.Join();
+        }
+
+        public static void Counting()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Current count: {i} on thread: {Thread.CurrentThread.ManagedThreadId}.");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
