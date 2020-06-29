6ef3afec98bc8402547532791947fbf71ab74edd
Dan Dumitriu <dandumitriu33@gmail.com>
2020-06-29 19:24:41 +0300

    Add Thread Pool test

---
+++ SimpleThreadingDemo/ThreadPoolDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(ShowMyText);
+            ThreadPool.QueueUserWorkItem(ShowMyText);
+            ThreadPool.QueueUserWorkItem(ShowMyText);
+            ThreadPool.QueueUserWorkItem(ShowMyText);
+            ThreadPool.QueueUserWorkItem(ShowMyText);
+            ThreadPool.QueueUserWorkItem(ShowMyText);
+            ThreadPool.QueueUserWorkItem(ShowMyText);
+            ThreadPool.QueueUserWorkItem(ShowMyText);
+            ThreadPool.QueueUserWorkItem(ShowMyText);
+            Console.WriteLine("Main thread does some work, then sleeps.");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Main thread exits.");
+        }
+
+        public static void ShowMyText(object state)
+        {
+            var temp = (string) state;
+            temp += $" {Thread.CurrentThread.ManagedThreadId}";
+            Console.WriteLine(temp);
+        }
+    }
+}
