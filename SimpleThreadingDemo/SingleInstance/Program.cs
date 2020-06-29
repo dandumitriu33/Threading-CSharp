dd065760e70371fc5ea502268e931a5f6f425dba
Dan Dumitriu <dandumitriu33@gmail.com>
2020-06-29 19:03:45 +0300

    Add 2nd exercise project SingleInstance - fail

---
+++ SimpleThreadingDemo/SingleInstance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SingleInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex mut = null;
+            const string mutexName = "RUNMEONLYONCE";
+
+            while (1 == 1)
+            {
+                try
+                {
+                    mut = Mutex.OpenExisting(mutexName);
+                }
+                catch (WaitHandleCannotBeOpenedException)
+                {
+                    Console.WriteLine("Mutex does not exist.");
+                    Thread.Sleep(1000);
+                    Mutex RUNMEONLYONCE = new Mutex();
+                }
+            }
+
+            mut.ReleaseMutex();
+            mut.Dispose();
+
+        }
+    }
+}
